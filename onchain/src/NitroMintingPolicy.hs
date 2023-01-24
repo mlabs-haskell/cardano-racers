2e8c6fa89cba42b123c6275108a6424c4a275f1d
Vladimir Kalnitsky <vladimir@mlabs.city>
2023-01-24 18:52:13 +0400

    Initialize the project

---
+++ onchain/src/NitroMintingPolicy.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -w #-}
+module NitroMintingPolicy (script) where
+
+import PlutusTx.Prelude
+
+import Ledger (PaymentPubKeyHash (unPaymentPubKeyHash))
+import Plutus.V2.Ledger.Api (Script, ScriptContext (scriptContextTxInfo), fromCompiledCode, getPubKeyHash)
+import Plutus.V2.Ledger.Contexts (txSignedBy)
+import PlutusTx (unsafeFromBuiltinData)
+import PlutusTx qualified (compile)
+
+{-# INLINEABLE mkValidator #-}
+mkValidator :: () -> () -> ScriptContext -> Bool
+mkValidator _datum _redeemer ctx =
+  traceIfFalse errMessage True
+  where
+    errMessage = "Failed verification"
+
+{-# INLINEABLE mkValidator' #-}
+mkValidator' :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkValidator' datum redeemer context =
+  let
+    result = mkValidator
+      (unsafeFromBuiltinData datum)
+      (unsafeFromBuiltinData redeemer)
+      (unsafeFromBuiltinData context)
+  in
+    if result then () else traceError "Failed verification"
+
+script :: Script
+script = fromCompiledCode $$(PlutusTx.compile [||mkValidator'||])
