97619259635bedb72e09f205a4c03d60e97ec9da
Amir Rad <ahmr10@gmail.com>
2023-05-04 00:10:17 +0100

    Rework RegisrtyScript

--- onchain/src/RaceRegistryScript.hs
+++ onchain/src/RaceRegistryScript.hs
@@ -1,18 +1,26 @@
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-specialise #-}
 
 module RaceRegistryScript (script) where
 
-import CommonTypes (RacersParams (RacersParams), adminToken)
-import Constants (nitroTokenName, slotTokenName)
-import Ledger.Address (scriptHashAddress)
+import CommonTypes (RacersParams (RacersParams), adminToken, botToken)
+import Constants (nitroTokenName)
+import Data.Eq qualified (Eq)
+import Data.Function (on)
+import GHC.Generics
+import GHC.Show (Show)
 import Plutonomy qualified (optimizeUPLC)
-import Plutus.V1.Ledger.Value (assetClass, assetClassValue, assetClassValueOf, flattenValue, geq, mpsSymbol)
+import Plutus.V1.Ledger.Value (AssetClass, assetClass, assetClassValue, assetClassValueOf, geq, mpsSymbol)
 import Plutus.V2.Ledger.Api (
+  Address,
   MintingPolicyHash,
+  PubKeyHash,
   Script,
   ScriptContext,
-  TokenName (unTokenName),
+  ToData (toBuiltinData),
+  TokenName,
   TxInInfo (txInInfoResolved),
   TxInfo (txInfoInputs),
   TxOut (txOutAddress),
@@ -21,77 +29,169 @@ import Plutus.V2.Ledger.Api (
   scriptContextTxInfo,
   txInfoMint,
  )
-import Plutus.V2.Ledger.Contexts (ownHash, valueLockedBy, valueSpent)
+import Plutus.V2.Ledger.Contexts (findOwnInput, getContinuingOutputs, txSignedBy, valueSpent)
 import Plutus.V2.Ledger.Tx (TxOut (txOutValue))
 import PlutusTx qualified (compile, unsafeFromBuiltinData, unstableMakeIsData)
+import PlutusTx.Builtins (serialiseData)
 import PlutusTx.Prelude
+import Utils (getInlineDatumFromTxOut)
+
+data RaceParticipant = RaceParticipant
+  { car :: TokenName
+  , driver :: TokenName
+  , payoutAddress :: Address
+  }
+  deriving (Data.Eq.Eq, Show, Generic)
+PlutusTx.unstableMakeIsData ''RaceParticipant
+
+instance Eq RaceParticipant where
+  RaceParticipant car1 driver1 payoutAddress1 == RaceParticipant car2 driver2 payoutAddress2 =
+    car1 == car2 && driver1 == driver2 && payoutAddress1 == payoutAddress2
+
+-- TODO: find a better way to implement/derive this
+instance Ord RaceParticipant where
+  compare = compare `on` (serialiseData . toBuiltinData)
+
+data RegistryEntry = PendingSelection PubKeyHash | ConfirmedSelection RaceParticipant
+  deriving (Data.Eq.Eq, Show, Generic)
+PlutusTx.unstableMakeIsData ''RegistryEntry
+
+instance Eq RegistryEntry where
+  PendingSelection pkh1 == PendingSelection pkh2 = pkh1 == pkh2
+  ConfirmedSelection rp1 == ConfirmedSelection rp2 = rp1 == rp2
+  _ == _ = False
+
+-- TODO: find a better way to implement/derive this
+instance Ord RegistryEntry where
+  compare = compare `on` (serialiseData . toBuiltinData)
+
+newtype RegistryDatum = RegistryDatum [RegistryEntry]
+PlutusTx.unstableMakeIsData ''RegistryDatum
+
+data RegistryRedeemer = Enroll [PubKeyHash] | SelectAssets | Collect
+PlutusTx.unstableMakeIsData ''RegistryRedeemer
 
 data RegistryParams = RegistryParams
-  { raceHash :: BuiltinByteString
-  , nitroFee :: Integer
+  { slotAssetClass :: AssetClass
   , nitroPolicyHash :: MintingPolicyHash
+  , gameAssetPolicyHash :: MintingPolicyHash
+  , nitroFee :: Integer
   }
 PlutusTx.unstableMakeIsData ''RegistryParams
 
--- | The `RaceRegistryScript` holds minted Slot tokens that can be purchased by
--- | users. It allows users to spend Slot tokens provided they burn the
--- | parameter `nitroFee` of NITRO tokens.
 {-# INLINEABLE mkRegistryScript #-}
-mkRegistryScript :: RacersParams -> RegistryParams -> ScriptContext -> Bool
+mkRegistryScript :: RacersParams -> RegistryParams -> RegistryRedeemer -> ScriptContext -> Bool
 mkRegistryScript
-  RacersParams {adminToken}
-  RegistryParams {nitroFee, nitroPolicyHash}
-  ctx =
-    inputContainsAdminNft
-      || traceIfFalse "bad amount of nitro burnt" burnsNitroPerSlotPurchased -- to collect leftover min-ada UTxO's
+  RacersParams {adminToken, botToken}
+  RegistryParams {slotAssetClass, nitroPolicyHash, gameAssetPolicyHash, nitroFee}
+  red
+  ctx = case red of
+    Collect ->
+      traceIfFalse "admin token not present" inputContainsAdminNft
+        || traceIfFalse "bot token not present" inputContainsBotNft
+      where
+        inputContainsAdminNft :: Bool
+        inputContainsAdminNft = valueSpent info `geq` assetClassValue adminToken 1
+
+        inputContainsBotNft :: Bool
+        inputContainsBotNft = valueSpent info `geq` assetClassValue botToken 1
+    Enroll pkhs ->
+      traceIfFalse "value at registry not conserved" valueAtRegistryIsConserved
+        && traceIfFalse "existing regitry entries must not be altered" doesNotAlterExistingEntries
+        && traceIfFalse "only one new signer entry must be added" addsPkhsToRegistry
+        && traceIfFalse "does not burn enought NITRO" burnsNitroPerSlotPurchased
+      where
+        (removedEntries, _, addedEntries) = diffedRegistry
+
+        -- When removedEntries is empty, the input registry is a subset of the output registry and so the registry is unaltered
+        doesNotAlterExistingEntries :: Bool
+        doesNotAlterExistingEntries = null removedEntries
+
+        addsPkhsToRegistry :: Bool
+        addsPkhsToRegistry = all aux addedEntries
+          where
+            aux (PendingSelection pkh) = pkh `elem` pkhs
+            aux _ = False -- Only pending selections can be added
+        burnsNitroPerSlotPurchased :: Bool
+        burnsNitroPerSlotPurchased = assetClassValueOf (txInfoMint info) (assetClass (mpsSymbol nitroPolicyHash) nitroTokenName) <= totalNitro
+          where
+            totalNitro = nitroFee * length addedEntries
+    SelectAssets ->
+      traceIfFalse "value at registry not conserved" valueAtRegistryIsConserved
+        && traceIfFalse "not signed by all removed pending pubkey hashes" signedByRemovedEntries
+        && traceIfFalse "input does not contain all selected assets " inputContainsAssetSelections
+      where
+        (removedEntries, _, addedEntries) = diffedRegistry
+
+        signedByRemovedEntries :: Bool
+        signedByRemovedEntries = all aux removedEntries
+          where
+            aux (PendingSelection pkh) = txSignedBy info pkh
+            aux _ = False -- Only pending selections can be removed
+        inputContainsAssetSelections :: Bool
+        inputContainsAssetSelections = all aux addedEntries
+          where
+            spentValue = valueSpent info
+            gameAssetSymbol = mpsSymbol gameAssetPolicyHash
+            aux (ConfirmedSelection RaceParticipant {car, driver}) =
+              spentValue `geq` ((<>) `on` (flip assetClassValue 1 . assetClass gameAssetSymbol)) car driver
+            aux _ = False
     where
       info :: TxInfo
-      info = scriptContextTxInfo ctx
+      !info = scriptContextTxInfo ctx
 
-      inputContainsAdminNft :: Bool
-      inputContainsAdminNft = valueSpent info `geq` assetClassValue adminToken 1
+      inputBeingValidated :: TxOut
+      !inputBeingValidated = maybe (traceError "could not get own input") txInInfoResolved $ findOwnInput ctx
 
-      -- Checks that value minted is less than expected nitro burn fee
-      burnsNitroPerSlotPurchased :: Bool
-      burnsNitroPerSlotPurchased = assetClassValueOf (txInfoMint info) (assetClass (mpsSymbol nitroPolicyHash) nitroTokenName) <= totalNitro
+      ownAddress :: Address
+      !ownAddress = txOutAddress inputBeingValidated
+
+      ownInputs :: [TxOut]
+      !ownInputs = filter ((== ownAddress) . txOutAddress) $ map txInInfoResolved $ txInfoInputs info
+
+      singletonSlot :: Value
+      !singletonSlot = assetClassValue slotAssetClass 1
+
+      valueAtRegistryIsConserved :: Bool
+      !valueAtRegistryIsConserved = foldMap txOutValue (getContinuingOutputs ctx) `geq` foldMap txOutValue ownInputs
+
+      !inputRegistryEntries = foldMap snd inputRegistry
+      !outputRegistryEntries = foldMap snd outputRegistry
+
+      inputRegistry :: [(TxOut, [RegistryEntry])]
+      !inputRegistry = map (\txo -> (txo, getTxoRegistry txo)) $ filter ((`geq` singletonSlot) . txOutValue) ownInputs
+
+      outputRegistry :: [(TxOut, [RegistryEntry])]
+      !outputRegistry = map (\txo -> (txo, getTxoRegistry txo)) $ filter ((`geq` singletonSlot) . txOutValue) $ getContinuingOutputs ctx
+
+      diffedRegistry :: ([RegistryEntry], [RegistryEntry], [RegistryEntry])
+      !diffedRegistry = go (sort inputRegistryEntries) outputRegistryEntries [] [] []
         where
-          totalNitro = nitroFee * slotTokensSpentFromScript
+          go [] [] left commons right = (reverse left, reverse commons, reverse right)
+          go xs [] left commons right = (reverse xs ++ left, reverse commons, reverse right)
+          go [] ys left commons right = (reverse left, reverse commons, reverse ys ++ right)
+          go (x : xs) (y : ys) left commons right
+            | x < y = go xs (y : ys) (x : left) commons right
+            | x > y = go (x : xs) ys left commons (y : right)
+            | otherwise = go xs ys left (x : commons) right
 
-      -- Only checks TokenName of spent value since it is not possible to get
-      -- access to the Slot currency symbol due to cyclic dependency. It is up
-      -- to the user to ensure spending of the correct AssetClass. Enrollment
-      -- policy will reject minting of Contender tokens if Slot tokens policy
-      -- symbol doesn't match.
-      slotTokensSpentFromScript :: Integer
-      slotTokensSpentFromScript = case flattenValue valueSpentFromScript of
-        [(_, tk, amt)] ->
-          if tk == slotTokenName
-            then amt
-            else traceError $ "expected '" <> decodeUtf8 (unTokenName slotTokenName) <> "' token name"
-        _ -> traceError $ "expected single entry '" <> decodeUtf8 (unTokenName slotTokenName) <> "' token name spent"
-
-      valueSpentFromScript :: Value
-      valueSpentFromScript = totalScriptInputValue - totalScriptOutputValue
-        where
-          ownAddress = scriptHashAddress (ownHash ctx)
-          totalScriptInputValue =
-            foldMap (txOutValue . txInInfoResolved) $
-              filter ((==) ownAddress . txOutAddress . txInInfoResolved) $
-                txInfoInputs info
-          totalScriptOutputValue = valueLockedBy info (ownHash ctx)
+      getTxoRegistry :: TxOut -> [RegistryEntry]
+      getTxoRegistry txo = case getInlineDatumFromTxOut txo of
+        Just (RegistryDatum registry) -> registry
+        Nothing -> traceError "could not get registry from txo inline datum"
 
 {-# INLINEABLE mkScript #-}
-mkScript :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
-mkScript racersParams registryParams _dat _red ctx =
+mkScript :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkScript racersParams registryParams _raceHash _dat red ctx =
   let
     result =
       mkRegistryScript
         (PlutusTx.unsafeFromBuiltinData racersParams)
         (PlutusTx.unsafeFromBuiltinData registryParams)
-        -- (PlutusTx.unsafeFromBuiltinData red)
+        (PlutusTx.unsafeFromBuiltinData red)
         (PlutusTx.unsafeFromBuiltinData ctx)
    in
     if result then () else traceError "Failed verification"
 
 script :: Script
-script = fromCompiledCode $ Plutonomy.optimizeUPLC $$(PlutusTx.compile [||mkScript||])
+script = fromCompiledCode $ Plutonomy.optimizeUPLC $$(PlutusTx.compile [||mkScript'||])
