702e215d6b4af8929a6951acc5e1559f38e0aadb
Amir Rad <ahmr10@gmail.com>
2023-06-15 16:02:39 +0100

    Update typescript api types and check for syntax errors

--- api/Admin.ts
+++ api/Admin.ts
@@ -1,75 +1,87 @@
-import "./Common.ts"
-import { Address, AssetPrices, Lovelace, RacersQueries, TransactionHash } from "./Common";
+import "./Common.ts";
+import {
+  Address,
+  AssetPrices,
+  ContractParams,
+  Lovelace,
+  RacersUtils,
+  RacersParams,
+  RacersQueries,
+  TransactionHash,
+  WalletSpec,
+  InitialState,
+} from "./Common";
 
 // Runs in the browser, connects to a browser wallet and Admin functions,
 // such racers instant initialization, onchain state modifications and race
 // creation.
 
-
-interface Admin extends RacersQueries {
-
-  // static async initClient(cfg: ContractConfig, racersParams: RacersParams, walletId: WalletId): Promise<Admin>;
-
-  /*
-  * - Mints AdminNFT, BotNFT, StateNFT.
-  * - Pays AdminNFT to admin address
-  * - Creates reference scripts for NitroPolicy, AssetRequestPolicy, 
-  *   DepositScript, DriverAssetPolicy, CarAssetPolicy
-  * - Sets initial racers state
-  */
-  // static async initRacers(cfg: ContractConfig, initialState: InitialState, credentialProvider: Wallet): Promise<RacersParams>;
-
-  // writes (Admin only)
-
-
+export interface MkAdmin extends RacersUtils {
+  /**
+   * Initializes a Contract environment for the given contract parameters and
+   * wallet spec. Returns an instance of Admin when initialized.
+   */
+  mkAdmin: (
+    cp: ContractParams,
+    w: WalletSpec,
+    racersParams: RacersParams
+  ) => Admin;
 
   /**
-  * Sets the onchain nitro price to the given value
-  * @param {Lovelace} p The new nitro price in Lovelace
-  * @returns The transaction hash of the set transaction
-  */
+   * initRacers - Initialize a Racers instance. Mints AdminNFT, BotNFT, StateNFT.
+   * Pays StateNFT to state script. Creates reference scripts for NitroPolicy, AssetRequestPolicy, DriverAssetPolicy, CarAssetPolicy, DepositScript
+   *
+   * @param {WalletSpec} walletSpec - Specifies the wallet for the operation.
+   * @param {InitialStateFFI} initialState - The initial state for the Racers instance, which includes:
+   * - treasuryAddress: The address of the treasury
+   * - operatingAddress: The operating address
+   * - assetPrices: The initial asset prices
+   * - nitroPrice: The initial price of nitro
+   *
+   * @returns {Promise<RacersParams>} - Returns a promise that resolves to the parameters of the created Racers instance.
+   */
+  initRacers: (
+    cp: ContractParams,
+    w: WalletSpec,
+    initialState: InitialState
+  ) => Promise<RacersParams>;
+}
+
+export interface Admin extends RacersQueries {
+  /*
+   * - Mints AdminNFT, BotNFT, StateNFT.
+   * - Pays AdminNFT to admin address
+   * - Creates reference scripts for NitroPolicy, AssetRequestPolicy,
+   *   DepositScript, DriverAssetPolicy, CarAssetPolicy
+   * - Sets initial racers state
+   */
+  // static async initRacers(cfg: ContractConfig, initialState: InitialState, credentialProvider: Wallet): Promise<RacersParams>;
+
+  /**
+   * Sets the onchain nitro price to the given value
+   * @param {Lovelace} p The new nitro price in Lovelace
+   * @returns The transaction hash of the set transaction
+   */
   setNitroPrice(p: Lovelace): Promise<TransactionHash>;
 
   /**
-  * Sets the onchain asset prices to the given values
-  * @param {AssetPrices} ap The new asset prices by rarity class in Lovelace
-  * @returns The transaction hash of the set transaction
-  */
+   * Sets the onchain asset prices to the given values
+   * @param {AssetPrices} ap The new asset prices by rarity class in Lovelace
+   * @returns The transaction hash of the set transaction
+   */
   setAssetPrices(ap: AssetPrices): Promise<TransactionHash>;
 
   /**
-  * Sets the onchain treasury address to the given value
-  * @param {Address} ta The new treasury address
-  * @returns The transaction hash of the set transaction
-  */
+   * Sets the onchain treasury address to the given value
+   * @param {Address} ta The new treasury address
+   * @returns The transaction hash of the set transaction
+   */
   setTreasuryAddress(ta: Address): Promise<TransactionHash>;
 
   /**
-  * Sets the onchain operating address to the given value
-  * @param {Address} oa The new operating address
-  * @returns The transaction hash of the set transaction
-  */
+   * Sets the onchain operating address to the given value
+   * @param {Address} oa The new operating address
+   * @returns The transaction hash of the set transaction
+   */
   setOperatingAddress(oa: Address): Promise<TransactionHash>;
-
-  // Admin only
-  // async createRace(race: Race, slots: number): Promise<void>
-
-  // close race
 }
-
-/**
- * initRacers - Initialize a Racers instance. Mints AdminNFT, BotNFT, StateNFT.
- * Pays StateNFT to state script. Creates reference scripts for NitroPolicy, AssetRequestPolicy, DriverAssetPolicy, CarAssetPolicy, DepositScript
- * 
- * @param {WalletSpec} walletSpec - Specifies the wallet for the operation.
- * @param {InitialStateFFI} initialState - The initial state for the Racers instance, which includes:
- * - treasuryAddress: The address of the treasury
- * - operatingAddress: The operating address
- * - assetPrices: The initial asset prices
- * - nitroPrice: The initial price of nitro
- * 
- * @returns {Promise<RacersParams>} - Returns a promise that resolves to the parameters of the created Racers instance.
- */
-function initRacers(walletSpec: WalletSpec, initialState: InitialStateFFI): Promise<RacersParams>;
-
-
--- api/Client.ts
+++ api/Client.ts
@@ -1,54 +1,63 @@
-import { Nitro, Race, RacersQueries, Rarity, TokenName, TransactionHash } from './Common';
+import {
+  ContractParams,
+  Nitro,
+  Race,
+  RacersParams,
+  RacersQueries,
+  RacersUtils,
+  Rarity,
+  TokenName,
+  TransactionHash,
+  WalletSpec,
+} from "./Common";
 
 // Runs in the browser, connects to a browser wallet and exposes racers
 // specific functionality
+export interface MkClient extends RacersUtils {
+  /**
+   * Initializes a Contract environment for the given contract parameters and
+   * wallet spec. Returns an instance of Client when initialized.
+   */
+  mkClient: (cp: ContractParams, w: WalletSpec, rp: RacersParams) => Client;
+}
 
 interface Client extends RacersQueries {
   /**
-  * Attempst to buy the given amount of Nitro using the current nitro price
-  * from the blockchain
-  * @param {Nitro} amount The amount of Nitro to buy
-  * @returns The transaction hash of the buy transaction
-  */
+   * Attempst to buy the given amount of Nitro using the current nitro price
+   * from the blockchain
+   * @param {Nitro} amount The amount of Nitro to buy
+   * @returns The transaction hash of the buy transaction
+   */
   buyNitro(amount: Nitro): Promise<TransactionHash>;
 
   /**
-  * Requests an asset by rarity, using the current asset prices on the
-  * blockchain
-  * @param {Rarity} rarity The rarity of the asset to request
-  * @returns The transaction hash of the request transaction
-  */
+   * Requests an asset by rarity, using the current asset prices on the
+   * blockchain
+   * @param {Rarity} rarity The rarity of the asset to request
+   * @returns The transaction hash of the request transaction
+   */
   requestAsset(rarity: Rarity): Promise<TransactionHash>;
 
   /**
-  * Registers in the given race by burning the nitro fee and inserting
-  * a 'registered' pkh entry at the race registry script
-  * @param {Race} race The race to register in identified by raceId and nitroFee
-  * @returns The transaction hash of the register transaction
-  */
+   * Registers in the given race by burning the nitro fee and inserting
+   * a 'registered' pkh entry at the race registry script
+   * @param {Race} race The race to register in identified by raceId and nitroFee
+   * @returns The transaction hash of the register transaction
+   */
   registerInRace(race: Race): Promise<TransactionHash>;
 
   /**
-  * Confirms asset selection for race participation by providing the token
-  * names for the chosen car and driver. Looks for the assets in the current
-  * wallet and modifies the users 'registered' entry into an 'assetSelection'
-  * @param {Race} race The race to register in identified by raceId and nitroFee
-  * @param {TokenName} car The token name of the chosen car
-  * @param {TokenName} driver The token name of the chosen driver
-  * @returns The transaction hash of the confirm transaction
-  */
-  joinRace(race: Race, car: TokenName, driver: TokenName): Promise<TransactionHash>;
+   * Confirms asset selection for race participation by providing the token
+   * names for the chosen car and driver. Looks for the assets in the current
+   * wallet and modifies the users 'registered' entry into an 'assetSelection'
+   * @param {Race} race The race to register in identified by raceId and nitroFee
+   * @param {TokenName} car The token name of the chosen car
+   * @param {TokenName} driver The token name of the chosen driver
+   * @returns The transaction hash of the confirm transaction
+   */
+  joinRace(
+    race: Race,
+    car: TokenName,
+    driver: TokenName
+  ): Promise<TransactionHash>;
 }
-
-type AssetMetadata = {
-  name: Cip25String;
-  assetType: AssetType;
-  imageUrl: String;
-  description: String;
-  attributes: any;
-}
-
-type AssetType = "driver" | "car"
-
-
-type WalletId = "nami" | "gerowallet" | "flint" | "LodeWallet" | "eternl"
