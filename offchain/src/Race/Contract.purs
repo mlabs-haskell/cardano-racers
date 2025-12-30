f8e4a2fc0ed027c135700d611475ae9bf9f34e68
Dzmitry Shuiski <errfrom@yandex.ru>
2025-12-30 12:36:51 +0100

    Implement distributeRewards offchain contract

--- offchain/src/Race/Contract.purs
+++ offchain/src/Race/Contract.purs
@@ -1,28 +1,42 @@
 module CardanoRacers.Race.Contract
-  ( mkRaceValidator
+  ( distributeRewards
+  , mkRaceValidator
   , startRace
   ) where
 
 import Contract.Prelude
 
+import Cardano.FromData (fromData)
 import Cardano.Plutus.ApplyArgs (applyArgs)
 import Cardano.Plutus.Types.Address (Address) as Plutus
-import Cardano.Plutus.Types.Value (fromCardano) as Plutus.Value
+import Cardano.Plutus.Types.Map (toCardano) as Plutus.Map
+import Cardano.Plutus.Types.Value (fromCardano, toCardano) as Plutus.Value
 import Cardano.ToData (toData)
 import Cardano.Types
-  ( AssetName
+  ( Address
+  , Asset(Asset)
+  , AssetName
+  , Credential(ScriptHashCredential)
   , Ed25519KeyHash
   , PlutusScript
+  , RedeemerDatum
+  , ScriptHash
   , TransactionHash
+  , TransactionInput
+  , TransactionOutput
   , Value
   )
-import Cardano.Types.BigNum (one) as BigNum
+import Cardano.Types.Address (mkPaymentAddress)
+import Cardano.Types.BigNum (fromInt, one, toBigInt) as BigNum
+import Cardano.Types.OutputDatum (outputDatumDatum)
 import Cardano.Types.PlutusScript (hash) as PlutusScript
+import Cardano.Types.Value (lovelaceValueOf, valueOf, valueToCoin)
 import Cardano.Types.Value (singleton) as Value
-import CardanoRacers.Helpers (mkPosixTimeUnsafe)
+import CardanoRacers.Helpers (mkPosixTimeUnsafe, paysToAddrConstraint)
 import CardanoRacers.Race.Types
-  ( RaceDatum(ValueEscrow, RaceState)
+  ( RaceDatum(ValueEscrow, RaceState, TokenBin)
   , RaceParams
+  , RaceRedeemer(DistributeRewards)
   , raceStateTokenName
   , valueEscrowTokenName
   )
@@ -31,19 +45,35 @@ import CardanoRacers.RaceSlot.Contract
   , mkRaceSlotPolicy
   )
 import CardanoRacers.RaceSlot.Types (RaceHash)
+import CardanoRacers.RacersState.Contract (queryRacersState)
+import CardanoRacers.RacersState.Types (RacersState(RacersState))
 import CardanoRacers.ScriptsFFI (raceScript)
+import Contract.Address (getNetworkId)
 import Contract.Chain (currentTime)
 import Contract.Monad (Contract, liftContractM)
 import Contract.ScriptLookups (ScriptLookups)
+import Contract.ScriptLookups (unspentOutputs, validator) as Lookups
 import Contract.TextEnvelope (decodeTextEnvelope, plutusScriptFromEnvelope)
 import Contract.Transaction (awaitTxConfirmed, submitTxFromConstraints)
 import Contract.TxConstraints (DatumPresence(DatumInline), TxConstraints)
-import Contract.TxConstraints (mustPayToScript) as Constraints
+import Contract.TxConstraints
+  ( mustPayToScript
+  , mustReferenceOutput
+  , mustSpendScriptOutput
+  ) as Constraints
+import Contract.Utxos (utxosAt)
+import Control.Monad.Error.Class (liftMaybe, throwError)
 import Control.Monad.Trans.Class (lift)
-import Data.Array (head) as Array
+import Data.Array (find, head) as Array
 import Data.Bifunctor (lmap)
+import Data.Int (ceil)
+import Data.Map (fromFoldable, toUnfoldable) as Map
+import Data.Newtype (wrap)
 import Data.Time.Duration (Days(Days))
+import Data.Traversable (traverse)
+import Data.Tuple (uncurry)
 import Effect.Exception (error)
+import JS.BigInt (toNumber) as BigInt
 import Partial.Unsafe (unsafePartial)
 import Racers (Racers)
 
@@ -122,6 +152,146 @@ startRace raceHash totalRewardValue participants delegates = do
       { txHash
       , raceParams
       }
+
+distributeRewards :: RaceParams -> Racers TransactionHash
+distributeRewards raceParams = do
+  RacersState { treasuryAddress, operatingAddress } /\ racersStateOref /\
+    racersStateTxOut <- queryRacersState
+
+  network <- lift getNetworkId
+  raceValidator <- lift $ mkRaceValidator raceParams
+  let
+    stateCurrencySymbol = (unwrap raceParams).stateCurrencySymbol
+    raceValidatorHash = PlutusScript.hash raceValidator
+    raceValidatorAddress =
+      mkPaymentAddress network (wrap $ ScriptHashCredential raceValidatorHash)
+        Nothing
+
+  { raceStateUtxo, valueEscrowUtxo } <- lift $ findRaceStateAndValueEscrowUtxos
+    raceValidatorAddress
+    stateCurrencySymbol
+
+  raceStateDatum <-
+    liftMaybe (error "Could not decode RaceDatum") $
+      decodeRaceDatum (snd raceStateUtxo)
+  rewardDistr <-
+    case raceStateDatum of
+      RaceState { distribution: Just distr } ->
+        liftMaybe (error "Could not convert Plutus distribution to Map") $
+          Plutus.Map.toCardano distr
+      RaceState { distribution: Nothing } ->
+        throwError $ error "Reward distribution not announced"
+      _ ->
+        throwError $ error
+          $ "Unexpected RaceDatum variant. Expected: RaceState, datum: "
+          <> show raceStateDatum
+
+  (rewards :: Array (Plutus.Address /\ Value)) <-
+    traverse
+      ( \(addr /\ plutusReward) ->
+          case Plutus.Value.toCardano plutusReward of
+            Just reward ->
+              pure $ addr /\ reward
+            Nothing -> do
+              throwError $ error
+                $ "Could not convert Plutus reward to Cardano.Value: "
+                <> show plutusReward
+      )
+      (Map.toUnfoldable rewardDistr)
+
+  let
+    mkStateTokenValue :: AssetName -> Value
+    mkStateTokenValue tn = Value.singleton stateCurrencySymbol tn BigNum.one
+
+    stateTokens :: Value
+    stateTokens =
+      unsafePartial
+        ( mkStateTokenValue raceStateTokenName
+            <> mkStateTokenValue valueEscrowTokenName
+        )
+
+    -- TODO: ensure these calculations are consistent with the on-chain validator
+    raceStateLovelace :: Number
+    raceStateLovelace = BigInt.toNumber $ BigNum.toBigInt $ unwrap $ valueToCoin
+      (unwrap $ snd raceStateUtxo).amount
+
+    treasuryValue :: Value
+    treasuryValue =
+      lovelaceValueOf $ BigNum.fromInt $ ceil $ raceStateLovelace * 0.75
+
+    operatingValue :: Value
+    operatingValue =
+      lovelaceValueOf $ BigNum.fromInt $ ceil $ raceStateLovelace * 0.25
+
+    redeemer :: RedeemerDatum
+    redeemer = wrap $ toData DistributeRewards
+
+    constraints :: TxConstraints
+    constraints = mconcat
+      [ Constraints.mustSpendScriptOutput (fst raceStateUtxo) redeemer
+      , Constraints.mustSpendScriptOutput (fst valueEscrowUtxo) redeemer
+      , foldMap (uncurry paysToAddrConstraint) rewards
+      , Constraints.mustPayToScript raceValidatorHash (toData TokenBin)
+          DatumInline
+          stateTokens
+      , Constraints.mustReferenceOutput racersStateOref
+      , paysToAddrConstraint treasuryAddress treasuryValue
+      , paysToAddrConstraint operatingAddress operatingValue
+      ]
+
+    lookups :: ScriptLookups
+    lookups = mconcat
+      [ Lookups.unspentOutputs $ Map.fromFoldable
+          [ raceStateUtxo
+          , valueEscrowUtxo
+          , racersStateOref /\ racersStateTxOut
+          ]
+      , Lookups.validator raceValidator
+      ]
+
+  lift do
+    txHash <- submitTxFromConstraints lookups constraints
+    awaitTxConfirmed txHash
+    pure txHash
+
+findRaceStateAndValueEscrowUtxos
+  :: Address
+  -> ScriptHash
+  -> Contract
+       { raceStateUtxo :: TransactionInput /\ TransactionOutput
+       , valueEscrowUtxo :: TransactionInput /\ TransactionOutput
+       }
+findRaceStateAndValueEscrowUtxos scriptAddr stateCs = do
+  scriptUtxos <- Map.toUnfoldable <$> utxosAt scriptAddr
+  raceStateUtxo <-
+    liftMaybe (error "Could not find RaceState utxo") $
+      Array.find
+        ( \(_ /\ txOut) ->
+            valueOf (Asset stateCs raceStateTokenName) (unwrap txOut).amount
+              == BigNum.one
+        )
+        scriptUtxos
+  valueEscrowUtxo <-
+    liftMaybe (error "Could not find ValueEscrow utxo") $
+      Array.find
+        ( \(_ /\ txOut) ->
+            valueOf (Asset stateCs valueEscrowTokenName) (unwrap txOut).amount
+              == BigNum.one
+        )
+        scriptUtxos
+  pure
+    { raceStateUtxo
+    , valueEscrowUtxo
+    }
+
+decodeRaceDatum :: TransactionOutput -> Maybe RaceDatum
+decodeRaceDatum txOut =
+  case (unwrap txOut).datum of
+    Just datum ->
+      case outputDatumDatum datum of
+        Just inlineDatum -> fromData inlineDatum
+        Nothing -> Nothing
+    Nothing -> Nothing
 
 mkRaceValidator :: RaceParams -> Contract PlutusScript
 mkRaceValidator params = do
